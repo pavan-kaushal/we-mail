a65dd0937cea484b8c9dc8f706e39e59c81d4915
pavan <pavan@inncircles.com>
2024-02-29 17:27:43 +0530

    authourisation service

--- src/app/components/authorisation/auth.service.ts
+++ src/app/components/authorisation/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { BaseService } from 'src/utils/base.service';
 
@@ -7,9 +8,18 @@ HttpClient
 @Injectable({providedIn: 'root'})
 export class AuthService extends BaseService {
     apiUrl = environment.apiUrl + '/auth';
-
+    authToken = new BehaviorSubject<string>(null);
     constructor(private _httpClient: HttpClient) {
         super(_httpClient);
+        
+    }
+
+    getAuthToken(){
+        const token = localStorage.getItem('authToken')
+        if(token){
+            this.authToken.next(token);
+        }
+        return this.authToken
     }
 
     signUpUser(data){
@@ -20,4 +30,17 @@ export class AuthService extends BaseService {
         return this.post<any>(this.apiUrl+'/signin',data)
     }
     
+    setRefreshToken(token){
+        localStorage.setItem('authToken',token)
+        this.authToken.next(token)
+    }
+
+    logOut(){
+        localStorage.removeItem('authToken')
+        this.authToken.next(null)
+    }
+
+    isTokenValid(){
+        
+    }
 }
